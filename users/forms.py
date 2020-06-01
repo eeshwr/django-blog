dc0a208ba5fcf138ab1787ac67fc39c187226dfb
ishwor <eeshwr@gmail.com>
2020-06-01 17:09:33 +0545

    implement generic view and pagination

--- users/forms.py
+++ users/forms.py
@@ -14,11 +14,13 @@ class UserRegisterForm(UserCreationForm):
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
+
     class Meta:
         model = User
-        fields = ['username','email']
+        fields = ['username', 'email']
+
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields =['image']
+        fields = ['image']
